9042e473f9072cb815d1f8092a88e83ebc6f39d1
Raymondkingjnr <raymondnnaji660@gmail.com>
2023-10-09 01:14:09 +0100

    firebase-vote-test-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
 
 function App() {
+  const firebaseConfig = {
+    apiKey: "AIzaSyApIa7VlHXM6MDIR6f0JhnJR-7-h6cJJk8",
+    authDomain: "vote-testing-app.firebaseapp.com",
+    projectId: "vote-testing-app",
+    storageBucket: "vote-testing-app.appspot.com",
+    messagingSenderId: "501590580393",
+    appId: "1:501590580393:web:93bef419f3d5afd4c8ddb8",
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  const db = getFirestore(app);
+
+  const colRef = collection(db, "candidates");
+
+  getDocs(colRef).then((Snapshot) => {
+    console.log(Snapshot.docs);
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello world</h1>
     </div>
   );
 }
